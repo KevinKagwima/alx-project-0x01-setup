bc44d7907b7185a689ed7e437d13af41f1c62989
KevinKagwima <KevinKagwima+github1@gmail.com>
2025-09-19 14:59:53 +0300

    updated files

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,6 +1,9 @@
+import Header from "@/components/layout/Header";
+
 const Posts: React.FC = () => {
   return (
     <>
+      <Header />
       <h1>This is the posts page</h1>
     </>
   );
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,6 +1,9 @@
+import Header from "@/components/layout/Header";
+
 const Users: React.FC = () => {
   return (
     <>
+      <Header />
       <h1>This is the users page</h1>
     </>
   );
