966bb1119caaca7d9dfcd5e3c562f6f0f4abfa25
KevinKagwima <KevinKagwima+github1@gmail.com>
2025-09-21 21:26:39 +0300

    updated files

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -4,3 +4,32 @@ export interface PostProps {
   title: string;
   body: string;
 }
+
+export interface UserProps {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
+  address: Address;
+  company: Company;
+}
+
+export interface Address {
+  street: string;
+  suite: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+export interface Geo {
+  lat: string;
+  lng: string;
+}
+
+export interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,12 +1,50 @@
 import Header from "@/components/layout/Header";
+import UserCard from "@/components/common/UserCard";
+import { UserProps } from "@/interfaces";
 
-const Users: React.FC = () => {
+const Users: React.FC<UserProps[]> = ({ users }) => {
   return (
     <>
       <Header />
-      <h1>This is the users page</h1>
+      {users?.map(
+        (
+          {
+            id,
+            name,
+            username,
+            email,
+            phone,
+            website,
+            address,
+            company,
+          }: UserProps,
+          key: number
+        ) => (
+          <UserCard
+            id={id}
+            name={name}
+            email={email}
+            username={username}
+            phone={phone}
+            website={website}
+            address={address}
+            company={company}
+          />
+        )
+      )}
     </>
   );
 };
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
+
+  return {
+    props: {
+      users,
+    },
+  };
+}
 
 export default Users;
